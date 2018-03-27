53219ae3db25cbaa4ad407102eb322c1c6b0df80
ChristopherMichael-Stokes <christophermichaelstokes@gmail.com>
2018-03-27 20:03:24 +0100

    Now detects all forms and allows the user to choose one at startup

--- src/bapers/BAPERS.java
+++ src/bapers/BAPERS.java
@@ -6,20 +6,29 @@
 package bapers;
 
 import bapers.data.domain.Staff;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.swing.JOptionPane;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.Resource;
 
 /**
  *
  * @author chris
  */
 public class BAPERS extends Application {
-    
+
     /**
      *
      */
@@ -30,22 +39,38 @@ public class BAPERS extends Application {
      *
      */
     public static Staff USER;
+    private static URL url
+            = BAPERS.class.getResource("/bapers/userInterface/fxml/Login.fxml");
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) throws IOException {
+        PathMatchingResourcePatternResolver scanner = new PathMatchingResourcePatternResolver();
+        Resource[] resources = scanner.getResources("/bapers/userInterface/fxml/*.fxml");
+        String[] options = Arrays.stream(resources).map(r -> r.getFilename()).toArray(String[]::new);
+        String selection;
+        Map<String, Resource> forms = new HashMap<>();
+        for (int i = 0; i < resources.length; ++i) {
+            forms.put(options[i], resources[i]);
+        }
+        selection = (String) JOptionPane.showInputDialog(
+                null, "Select a form", "form",
+                JOptionPane.QUESTION_MESSAGE, null,
+                options, null);
+        if (selection != null) {
+            url = forms.get(selection).getURL();
+            launch(args);
+        }
+        Platform.exit();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/bapers/userInterface/fxml/Login.fxml"));
-        Scene scene = new Scene(root, 800, 450);        
-        scene.getStylesheets().add("/bapers/userInterface/styles/login.css");
-
-        primaryStage.setTitle("Staff Types");
+        Parent root = FXMLLoader.load(url);
+        Scene scene = new Scene(root, 800, 450);
         primaryStage.setScene(scene);
-        primaryStage.show();    
+        primaryStage.show();
+
     }
 }
